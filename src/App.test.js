8d7f1672217cd9c16fa25b1b651819f638cc7b63
qkdwngus <arkhyeon@gmail.com>
2023-05-25 16:12:17 +0900

    Test and code gray background for disabled button

--- src/App.test.js
+++ src/App.test.js
@@ -23,29 +23,33 @@ test("button has correct initial color and updates when clicked button", () => {
   expect(colorButton).toHaveTextContent("Change to red");
 });
 
-test('initial conditions',()=>{
-  render(<App/>);
+test("initial conditions", () => {
+  render(<App />);
   // 버튼 활성화 상태로 시작
-  const colorButton = screen.getByRole('button', {name : 'Change to blue'});
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
   expect(colorButton).toBeEnabled();
   // 체크박스가 체크되지 않은 상태로 시작
 
-  const checkbox = screen.getByRole('checkbox');
+  const checkbox = screen.getByRole("checkbox");
   expect(checkbox).not.toBeChecked();
-})
+});
 
 test("button turns disabled property when toggle checkbox", () => {
   render(<App />);
 
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
-  const checkbox = screen.getByLabelText('Change Disable Property')
+  const checkbox = screen.getByRole("checkbox", {
+    name: "Change Disable Property",
+  });
+  // const checkbox = screen.getByLabelText('Change Disable Property')
 
   fireEvent.click(checkbox);
   expect(checkbox).toBeChecked();
   expect(colorButton).toBeDisabled();
+  expect(colorButton).toHaveStyle("background-color:gray");
 
   fireEvent.click(checkbox);
   expect(checkbox).not.toBeChecked();
   expect(colorButton).toBeEnabled();
-
+  expect(colorButton).not.toHaveStyle("background-color:gray");
 });
