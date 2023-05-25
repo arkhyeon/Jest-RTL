bbeff4209d46c4644fafc3158db0f238511b5256
qkdwngus <arkhyeon@gmail.com>
2023-05-25 15:33:24 +0900

    Test and code button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import { logRoles } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+test("button has correct initial color and updates when clicked button", () => {
+  const { container } = render(<App />);
+
+  logRoles(container);
+
+  // 이름이 'Change to blue'인 버튼 요소 찾기
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //배경색이 빨간색인지 확인
+  expect(colorButton).toHaveStyle("background-color:red");
+
+  // 버튼 클릭
+  fireEvent.click(colorButton);
+
+  // 배경이 파란색으로 변경
+  expect(colorButton).toHaveStyle("background-color:blue");
+
+  // 텍스트 변경 확인
+  expect(colorButton).toHaveTextContent("Change to red");
 });
+
+// test("button turns blue when clicked", () => {
+//   render(<App />);
+//
+//   const colorButton = screen.getByRole("button", { name: "Change to blue" });
+// });
